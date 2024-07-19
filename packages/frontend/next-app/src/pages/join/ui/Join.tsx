e2d619e0924e5e387abbd02a26f11cf09f1f5db6
JongMany <blackberry1114@naver.com>
2024-07-20 00:15:00 +0900

    chore: husky, lint-staged 제거 및 lefthook 설정

--- packages/frontend/next-app/src/pages/join/ui/Join.tsx
+++ packages/frontend/next-app/src/pages/join/ui/Join.tsx
@@ -1,7 +1,7 @@
 import { JoinCard } from "@/features/join/index.tsx";
 
 export default function Join() {
-  console.log("");
+  adasd;
   return (
     <main className="flex flex-col items-center pt-[72px] max-w-[640px] mx-auto">
       <h2 className="font-notoSansKr text-4xl font-bold text-primary mb-16">강쥐조아</h2>
