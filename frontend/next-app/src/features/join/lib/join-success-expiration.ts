31b5f6b4e987b9e85a81517ebc0d35f71d0e14d7
JongMany <blackberry1114@naver.com>
2024-07-24 00:02:59 +0900

    refactor: session-expiration 코드 리팩터링

--- frontend/next-app/src/features/join/lib/join-success-expiration.ts
+++ frontend/next-app/src/features/join/lib/join-success-expiration.ts
@@ -1,3 +1,8 @@
+import {
+  setExpirationStateInSessionStorage,
+  validateExpirationStateFromSessionStorage,
+} from "@/shared/lib/session-expiration-storage";
+
 const joinSuccessExpiration = 1000 * 60 * 30; // 30 minutes
 
 export type JoinSuccessItem<T> = {
@@ -6,27 +11,12 @@ export type JoinSuccessItem<T> = {
 };
 
 // 일정 시간 이상 지나면 삭제될 수 있는 정보를 담은 객체를 sessionStorage에 저장
-export function setJoinSuccessExpirationInSessionStorage<T>(value: T) {
-  const now = new Date();
-  const item = {
-    value,
-    expiry: now.getTime() + joinSuccessExpiration,
-  };
-  sessionStorage.setItem("joinSuccess", JSON.stringify(item));
+export function setJoinSuccessExpirationInSessionStorage(value: boolean) {
+  return setExpirationStateInSessionStorage("joinSuccess", joinSuccessExpiration)(value);
 }
 
 // 일정 시간 이상 지나면 삭제될 수 있는 정보를 담은 객체를 sessionStorage에 저장
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-constraint
-export function validateJoinSuccessFromSessionStorage<T extends unknown>() {
-  const itemStr = sessionStorage.getItem("joinSuccess");
-  if (!itemStr) {
-    return false;
-  }
-  const item = JSON.parse(itemStr) as JoinSuccessItem<T>;
-  const now = new Date();
-  if (now.getTime() > item.expiry) {
-    sessionStorage.removeItem("joinSuccess");
-    return false;
-  }
-  return item.value;
+export function validateJoinSuccessFromSessionStorage() {
+  return validateExpirationStateFromSessionStorage<boolean>("joinSuccess");
 }
